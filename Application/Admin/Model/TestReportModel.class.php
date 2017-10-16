879fe98e54c531de39b578b63adddc7a487a19fe
493955394 <493955394@qq.com>
2017-10-16 22:18:18 +0800

    add 2model

---
+++ Application/Admin/Model/TestReportModel.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ail
+ * Date: 2017/10/15
+ * Time: 16:38
+ */
+namespace Admin\Model;
+use Think\Model;
+class TestReportModel extends Model
+{
+    protected $tableName = 'test_reprot';
+
+}
