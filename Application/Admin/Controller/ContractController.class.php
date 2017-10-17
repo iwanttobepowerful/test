f7cf6ebed2d0f05888a5213b9909873245b418c8
493955394 <493955394@qq.com>
2017-10-17 09:53:34 +0800

    修改了外部签发读取的字段，线上数据库需要在test_reprot里增加两个字段clientSign,telephoneApplication/Admin/View/Contract/issueTestReport.html

--- Application/Admin/Controller/ContractController.class.php
+++ Application/Admin/Controller/ContractController.class.php
@@ -19,18 +19,60 @@ class ContractController extends Controller
         $this->assign('menu_secoud_active', strtolower(ACTION_NAME));
     }
     public function getTestReport(){
+        $page = I("p",'int');
+        $pagesize = 10;
+        if($page<=0) $page = 1;
+        $offset = ( $page-1 ) * $pagesize;
         $test_reprot=M("test_reprot");//实例化对象
         $where['authorizer']=1;
         $where['ifinnerissue']=0;
-        $rs=$test_reprot->where($where)->field('id,centreNo')->order('id')->select();//查找条件为已经批准并且内部尚未领取的报告
-        $this->assign('lists',$rs);
+        $rs=$test_reprot->where($where)->field('id,centreNo')->order('id')->limit("{$offset},{$pagesize}")->select();//查找条件为已经批准并且内部尚未领取的报告
+        $count = D("test_reprot")->where($where)->count();
+        $Page= new \Think\Page($count,$pagesize);
+        $Page->setConfig('theme',"<ul class='pagination'></li><li>%FIRST%</li><li>%UP_PAGE%</li><li>%LINK_PAGE%</li><li>%DOWN_PAGE%</li><li>%END%</li><li><a> %HEADER%  %NOW_PAGE%/%TOTAL_PAGE% 页</a></ul>");
+        $pagination= $Page->show();// 分页显示输出
+        $body = array(
+            'lists'=>$rs,
+            'pagination'=>$pagination,
+        );
+        $this->assign($body);
         $this->display();
     }
     //外部签发检验报告
     public function issueTestReport(){
-
+        $page = I("p",'int');
+        $pagesize = 10;
+        if($page<=0) $page = 1;
+        $offset = ( $page-1 ) * $pagesize;
+        $test_reprot=M("test_reprot");//实例化对象
+        $where['authorizer']=1;
+        $where['ifinnerissue']=1;
+        $where['ifouterissue']=0;
+        $rs=$test_reprot->where($where)->field('id,centreNo,productUnit,clientSign,telephone')->order('id')->limit("{$offset},{$pagesize}")->select();//查找条件为已经批准并且内部尚未领取的报告
+        $count = D("test_reprot")->where($where)->count();
+        $Page= new \Think\Page($count,$pagesize);
+        $Page->setConfig('theme',"<ul class='pagination'></li><li>%FIRST%</li><li>%UP_PAGE%</li><li>%LINK_PAGE%</li><li>%DOWN_PAGE%</li><li>%END%</li><li><a> %HEADER%  %NOW_PAGE%/%TOTAL_PAGE% 页</a></ul>");
+        $pagination= $Page->show();// 分页显示输出
+        $body = array(
+            'lists'=>$rs,
+            'pagination'=>$pagination,
+        );
+        $this->assign($body);
         $this->display();
     }
+    //标记签发报告，更新数据库
+    public function doUpd(){
+// 要修改的数据对象属性赋值
+        $data['ifouterissue'] = 1;
+        $id =I("id",0,'intval');
+        $rs = array("msg"=>"fail");
+        if(D("test_reprot")->where("id=".$id)->save($data)){
+            $rs['msg'] = 'succ';
+        }
+        $this->ajaxReturn($rs);
+    }
+
+
 
 //领取检验报告
 
