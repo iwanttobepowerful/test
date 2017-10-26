f28754d4aa16287f36a9aa60730a6c14e0229856
493955394 <493955394@qq.com>
2017-10-26 12:00:25 +0800

    增加手机端合同列表和修改密码两个界面

---
+++ Application/Wap/Controller/ContractController.class.php
@@ -0,0 +1,15 @@
+<?php
+namespace Wap\Controller;
+use Think\Controller;
+class ContractController extends Controller {
+    //初始化方法
+    public function _initialize(){
+        load('@.functions');
+        D("account")->checkLogin();
+        $this->assign('menu_active',strtolower(CONTROLLER_NAME));
+        $this->assign('menu_secoud_active',strtolower(ACTION_NAME));
+    }
+    public function contractList(){
+        $this->display();
+    }
+}
---
+++ Application/Wap/Controller/UserController.class.php
@@ -0,0 +1,54 @@
+<?php
+namespace Wap\Controller;
+use Think\Controller;
+class UserController extends Controller {
+    //初始化方法
+    public function _initialize(){
+        load('@.functions');
+        D("account")->checkLogin();
+        $this->assign('menu_active',strtolower(CONTROLLER_NAME));
+        $this->assign('menu_secoud_active',strtolower(ACTION_NAME));
+    }
+    //修改密码页面
+    public function updatePassword(){
+        $admin_info = M('common_system_user')->find($admin_id);
+        $this->assign('admin_info', $admin_info);
+        $this->display();
+    }
+    //保存修改信息
+    public function save(){
+        $admin_id          = I('id');
+        $admin_name          = I('username', '');
+        $old_admin_password  = md5(I('old_admin_password', ''));
+        $admin_password      =md5( I('admin_password', ''));
+        $re_admin_repassword = md5(I('re_admin_repassword', ''));
+
+        //是否一致
+        if($admin_password !== $re_admin_repassword){
+            $this->error('两次密码输入不一致');
+        }
+
+        //原始用户名和密码是否正确
+        $filter = array(
+            'username' => $admin_name,
+            'password' => $old_admin_password
+        );
+
+        $admin_info = M('common_system_user')->where($filter)->find();
+        if(!$admin_info){
+            $this->error('原始用户名或密码错误');
+        }else{
+            //更新管理员信息
+            $admin_info = array(
+                'password' => $re_admin_repassword
+            );
+            $result = M('common_system_user')->where(array('id' => $admin_id))->save($admin_info);
+            if($result){
+                $this->ajaxReturn(array('status' => 'ok', 'info' => '管理员信息修改成功'));
+            }else{
+                $this->ajaxReturn(array('status' => 'error', 'info' => '管理员信息修改失败'));
+            }
+        }
+
+    }
+}
