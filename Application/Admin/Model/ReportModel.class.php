3dd668b2ece23036dbaf5ac78cdd2b7b4dc78b87
493955394 <493955394@qq.com>
2017-10-28 15:38:00 +0800

    加了报告模板 Application/Admin/Controller/ReportController.class.php

---
+++ Application/Admin/Model/ReportModel.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ail
+ * Date: 2017/10/16
+ * Time: 11:13
+ */
+namespace Admin\Model;
+use Think\Model;
+class ReportModel extends Model
+{
+    protected $tableName = 'contract_flow';
+
+    public function checkStatus(){
+
+}
+
+}
