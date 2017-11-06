6eb7306387c2cfafeab4fccdb49f2ce45c848086
zwh <745778074@qq.com>
2017-11-06 15:28:14 +0800

    修改

--- Application/Admin/Controller/TestReportController.class.php
+++ Application/Admin/Controller/TestReportController.class.php
@@ -176,13 +176,13 @@ class TestReportController extends Controller
             $rs['msg'] = 'succ';
         }
         $this->ajaxReturn($rs);
- /*
-        $value = 'http://www.baidu.com'; //二维码内容
+
+        $value = 'http://www.baidu.com/'; //二维码内容
         $errorCorrectionLevel = 'L';//容错级别
         $matrixPointSize = 6;//生成图片大小
         //生成二维码图片
         QRcode::png($value, 'qrcode.png', $errorCorrectionLevel, $matrixPointSize, 2);
-        $logo = '__Public__/imgs/logo.png';//准备好的logo图片
+        $logo = '__PUBLIC__/imgs/logo.png';//准备好的logo图片
         $QR = 'qrcode.png';//已经生成的原始二维码图
 
         if ($logo !== FALSE) {
@@ -192,17 +192,16 @@ class TestReportController extends Controller
             $QR_height = imagesy($QR);//二维码图片高度
             $logo_width = imagesx($logo);//logo图片宽度
             $logo_height = imagesy($logo);//logo图片高度
-            $logo_qr_width = $QR_width / 3;
-            $scale = $logo_width / $logo_qr_width;
-            $logo_qr_height = $logo_height / $scale;
+            $logo_qr_width = $QR_width / 5;
+            $scale = $logo_width/$logo_qr_width;
+            $logo_qr_height = $logo_height/$scale;
             $from_width = ($QR_width - $logo_qr_width) / 2;
             //重新组合图片并调整大小
             imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width,
                 $logo_qr_height, $logo_width, $logo_height);
         }
         //输出图片
-        imagepng($QR, './Public/erweima/haha.png');
- */
+        imagepng($QR, '__PUBLIC__/imgs/1.png');
     }
 
 
