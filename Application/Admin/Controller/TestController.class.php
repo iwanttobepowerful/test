c77eabc0ebc982bc677e6a5c6ae72c0923574be8
xiao.Chen <cxbl1230@qq.com>
2017-11-22 00:24:02 +0800

    merge qrcode

--- Application/Admin/Controller/TestController.class.php
+++ Application/Admin/Controller/TestController.class.php
@@ -397,6 +397,7 @@ class TestController extends Controller{
             $result['msg'] = "无效的提交！";
             $this->ajaxReturn($result);
         }
+        $report = D("test_report")->where("centreno='{$centreno}'")->find();
         //pdf转换
         $distfile = convert2Pdf(ROOT_PATH,$data['path'],'pdf');
         $data = array(
@@ -407,11 +408,18 @@ class TestController extends Controller{
         );
         //转image,在测试服务器上测试，本地需要配置环境
         //demo
-        ///$distfile = "/Public/attached/2017-11-21/SJ-4-77_2017_01.pdf";        
+        //$distfile = "/Public/attached/2017-11-21/SJ-4-77_2017_01.pdf";        
         $imageFiles = convertPdf2Image(ROOT_PATH,$distfile);
         if($imageFiles){
             //转换成功,合并二维码
             
+            if(file_exists($imageFiles[0]) && file_exists($report['qrcode_path'])){
+                $baseinfo = pathinfo($imageFiles[0]);
+                $saveFile = $baseinfo['dirname'] . '/'.$baseinfo['filename'].'-tmp.'.$baseinfo['extension'];
+                mergeImage($imageFiles[0],$report['qrcode_path'],$saveFile);
+                @rename($saveFile,$imageFiles[0]);
+            }
+           
             //再转换成pdf
             $pdf = './Public/attached/report/'.$centreno.'.pdf';
             convertImageToPdf(ROOT_PATH,$pdf,$imageFiles);
