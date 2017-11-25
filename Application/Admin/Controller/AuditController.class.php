317e32bbb3aacd80a393806549ef25c5cd7b402a
493955394 <493955394@qq.com>
2017-11-25 11:53:16 +0800

    报告修改申请

--- Application/Admin/Controller/AuditController.class.php
+++ Application/Admin/Controller/AuditController.class.php
@@ -55,9 +55,9 @@ class AuditController extends Controller {
         $offset = ( $page-1 ) * $pagesize;
         if($de =='A'){//内部修改申请
         if(!empty($keyword)){
-            $where="r.centreno like '%{$keyword}% ' and r.if_outer=0";
+            $where="r.centreno like '%{$keyword}% ' and r.if_outer=0 and r.if_report=0";
         }else{
-            $where['r.if_outer']=0;
+            $where="r.if_outer and r.if_report=0";
         }
         }
         elseif($de =='B'){//报告修改申请
