c7a754c5ee4ad691f590e9fdd7e81a6c9dc09e3a
cxbl1230 <cxbl1230@qq.com>
2017-11-30 11:15:27 +0800

    ignore

--- Application/Admin/Conf/config.php
+++ Application/Admin/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//数据库配置'配置项'=>'配置值'
     'DB_TYPE'=>'mysql',
-    'DB_HOST'=>'101.200.140.10',
+    'DB_HOST'=>'47.104.5.218',
     'DB_NAME'=>'jiancai',
     'DB_USER'=>'jcuser',
-    'DB_PWD'=>'jC0090#usr!op',
+    'DB_PWD'=>'svipcn@cx.com',
     'DB_PORT'=>'3306',
     'DB_PREFIX' => '',
     //分页参数配置
