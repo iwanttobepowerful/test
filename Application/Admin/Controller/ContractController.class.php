645fab838e7bb796e47a1fbeaf1392f8b5746aa7
493955394 <493955394@qq.com>
2017-12-08 00:30:41 +0800

    领导查看

--- Application/Admin/Controller/ContractController.class.php
+++ Application/Admin/Controller/ContractController.class.php
@@ -1242,6 +1242,11 @@ class ContractController extends Controller
         }else{
             $if_edit = 0;
         }
+        if($user==8){
+          $leader=1;
+        }else{
+            $leader=0;
+        }
         //D("contract_flow as f");
         $keyword = I("keyword");//获取参数
         $keyword = trim($keyword);
@@ -1306,7 +1311,9 @@ class ContractController extends Controller
             'if_edit'=>$if_edit,
             'begin_time'=>$begin_time,
             'end_time'=>$end_time,
-            'keyword'=>$keyword
+            'keyword'=>$keyword,
+            'leader'=>$leader,
+            'if_admin'=>$if_admin
         );
         //dump($body);
         $this->assign($body);
